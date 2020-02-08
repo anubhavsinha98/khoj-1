3f1952d060a17b5f2e51afdd84e4eed0c186c9f4
Anubhav Sinha <anubhavsinha98@gmail.com>
2020-02-08 09:40:39 +0530

    Made changes

--- police/views.py
+++ police/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from guardian.models import *
+from core import recognize_faces_image
+import os
+from core import encode_faces
+import boto3
+import smtplib
 
 # Create your views here.
+def get_analysis(request):
+	return render(request, 'analyze.html')
